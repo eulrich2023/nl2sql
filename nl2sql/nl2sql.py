011363929b0dd0dcff6230c17111f98625e41574
Dimitar Petrunov <dimitar.petrunov@sagedata.net>
2023-05-16 02:25:32 +0300

    chore: rename requests_summary.py to req_summary.py

--- nl2sql/nl2sql.py
+++ nl2sql/nl2sql.py
@@ -27,7 +27,7 @@ from llama_index.indices.struct_store import SQLContextContainerBuilder
 from llama_index.prompts.prompts import TextToSQLPrompt
 from llama_index.storage.storage_context import StorageContext
 from llama_index.vector_stores import ChromaVectorStore
-from requests_summary import RequestsSummaryBuilder
+from req_summary import RequestsSummaryBuilder
 from streamlit.components.v1 import html
 from streamlit.logger import get_logger
 from streamlit_chat import message
